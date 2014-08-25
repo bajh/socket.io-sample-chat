c602ac380f70a29f34efaf55854a5d8c1889a537
Brandon Hopkins <brhpkns@gmail.com>
2014-08-24 21:39:52 -0400

    Chat form starts out as nickname input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http)
+var nicknames
 
 app.get('/', function(req, res){
   res.sendFile('index.html', {'root': './'});
@@ -11,11 +12,12 @@ app.get('/style.css', function(req, res){
 });
 
 io.on('connection', function(socket){
+  
   socket.broadcast.emit('chat message', '<<A new user has joined the room>>')
   socket.on('chat message', function(msg){
     socket.broadcast.emit('chat message', msg)
   });
-  socket.on('nickname submission', function(msg){
+  socket.on('nickname submission', function(nickname){
     console.log('nickname received');
   });
   socket.on('disconnect', function(){
