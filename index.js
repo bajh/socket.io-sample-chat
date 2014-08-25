e5fcaa469803eae91ffa4dda3a08804d15543b4d
Brandon Hopkins <brhpkns@gmail.com>
2014-08-25 12:31:53 -0400

    X

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var nicknames = {};
 console.log(process.env);
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   console.log("connection attempted")
