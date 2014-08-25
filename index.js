17ef7de2bf1d9c42a42c84c3669d0b3181ef3e0f
Brandon Hopkins <brhpkns@gmail.com>
2014-08-25 10:42:32 -0400

    Change port number to work with heroku

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
+var port = Number(process.env.port || 3000)
+http.listen(port, function() {
   console.log('listening on *:3000');
 });
