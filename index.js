4f84d5949995c21b5118f05e6fff117a241f0a9b
Brandon Hopkins <brhpkns@gmail.com>
2014-08-24 22:54:59 -0400

    Now with nicknames

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http)
-var nicknames
+var io = require('socket.io')(http);
+var nicknames = {};
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile('index.html', {'root': './'});
 });
 
-app.get('/style.css', function(req, res){
+app.get('/style.css', function(req, res) {
   res.sendFile('style.css', {'root': './'});
 });
 
-io.on('connection', function(socket){
-  
-  socket.broadcast.emit('chat message', '<<A new user has joined the room>>')
+io.on('connection', function(socket) {
+  socket.broadcast.emit('status message', '<<A new user has joined the room>>')
   socket.on('chat message', function(msg){
-    socket.broadcast.emit('chat message', msg)
+    socket.broadcast.emit('chat message', {"nickname": nicknames[socket.id], "message": msg});
   });
   socket.on('nickname submission', function(nickname){
-    console.log('nickname received');
+    nicknames[socket.id] = nickname;
   });
-  socket.on('disconnect', function(){
-    io.emit('chat message', '<<A user disconnected>>')
+  socket.on('disconnect', function() {
+    io.emit('status message', '<<' + nicknames[socket.id] + ' has signed off>>');
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
